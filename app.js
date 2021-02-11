7cfd5453e5e0d24d6565d6bb21d7dacba1e13b26
Mlks1981 <67844188+Mlks1981@users.noreply.github.com>
2021-02-11 14:49:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/proxy/',
+    prefix: '/course/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
