d51f6e19ecdaad9acbae3b4bcd6790cbf709d979
Mlks1981 <67844188+Mlks1981@users.noreply.github.com>
2021-02-17 22:02:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -48,12 +48,6 @@ function googleAnalyticsMiddleware(data) {
                 this.push(addGa(chunk.toString()));
                 next();
             }
-            function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
 }
         }));
     }
